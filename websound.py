7be75236be1a8cdcb080b9d2c7bf8e9b990d5346
drewp <drewp@bigasterisk.com>
2010-08-23 00:29:12 -0700

    add speech
    Ignore-this: 12ae976c166d80b3945db0d00754f4a1
    
    darcs-hash:20100823072912-312f9-f4396ce22028f65d5df9446a084a5f9228ee1608.gz

--- websound.py
+++ websound.py
@@ -5,20 +5,49 @@ play sounds according to POST requests. cooperate with pubsubhubbub
 """
 import web, sys, jsonlib, subprocess, os
 
+sensorWords = {"wifi" : "why fi",
+               "bluetooth" : "bluetooth"}
+
+def soundOut(preSound=None, speech=None, postSound=None):
+    if preSound:
+        subprocess.call(['aplay', preSound])
+
+    def playPost(action):
+        if action == 'end':
+            if postSound is not None:
+                subprocess.call(['aplay', postSound])
+
+    if speech:
+        speechClient.speak(speech, playPost)
+    else:
+        playPost('end')
+
+#def soundOut(preSound=None, speech=None, postSound=None):
+#    print vars()
+
+
 class visitorNet(object):
     def POST(self):
         data = jsonlib.loads(web.data())
+
         if data.get('action') == 'arrive':
+            
             snd = ('/my/music/entrance/%s.wav' %
                    data['name'].replace(' ', '_').replace(':', '_'))
-            if os.path.exists(snd):
-                subprocess.call(['aplay', snd])
-                return 'ok'
-            else:
-                return "no sound for %s" % snd
-        if data.get('action') == 'leave':
-            subprocess.call(['aplay', '/my/music/entrance/leave.wav'])
+            if not os.path.exists(snd):
+                snd = None
+
+            soundOut(speech="new %s: %s" % (sensorWords[data['sensor']],
+                                            data['name']),
+                     postSound=snd)
             return 'ok'
+
+        if data.get('action') == 'leave':
+            soundOut(preSound='/my/music/entrance/leave.wav',
+                     speech="lost %s. %s" % (sensorWords[data['sensor']],
+                                             data['name']))
+            return 'ok'
+        
         return "nothing to do"
 
 urls = (
@@ -26,6 +55,9 @@ urls = (
     )
 
 app = web.application(urls, globals(), autoreload=True)
+
+import speechd.client
+speechClient = speechd.client.Speaker("websound")
 
 if __name__ == '__main__':
     sys.argv.append("9049")
